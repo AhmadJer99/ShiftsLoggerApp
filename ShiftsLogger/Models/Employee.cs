1bc0c83db3471475ff1eb4337198875ddcffa3c6
Ahmad Jerjees <ajerjees010@gmail.com>
2025-01-04 15:34:22 +0300

    Added the models needed for deserializing

---
+++ ShiftsLogger/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ShiftsLoggerUI.Models;
+
+public class Employee
+{
+    [JsonProperty("empId")]
+    public int EmployeeId { get; set; }
+    [JsonProperty("empName")]
+    public string? EmployeeName { get; set; }
+    [JsonProperty("empPhone")]
+    public string? EmployeePhone { get; set; }
+    [JsonProperty("empShifts")]
+    public List<Shift>? Shifts { get; set; }
+}
---
+++ ShiftsLogger/Models/Shift.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ShiftsLoggerUI.Models;
+
+public class Shift
+{
+    [JsonProperty("shiftId")]
+    public int ShiftId { get; set; }
+    [JsonProperty("employee")]
+    public Employee? Employee { get; set; }
+    [JsonProperty("empId")]
+    public int EmpId { get; set; }
+    [JsonProperty("startDateTime")]
+    public DateTime? ShiftStartTime { get; set; }
+    [JsonProperty("endDateTime")]
+    public DateTime? ShiftEndTime { get; set; }
+    [JsonProperty("shiftDurationHours")]
+    public int ShiftDuration { get; set; }
+}
